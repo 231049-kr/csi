7caf4173f800c51a3cac807bbb3994296433f094
yamamoto shogo <231054@std.hi-joho.ac.jp>
2026-09-09 16:20:03 +0900

    tt

---
+++ test.py
@@ -0,0 +1,13 @@
+import numpy
+import pandas
+import matplotlib
+import scipy
+import sklearn
+
+print("NumPy:", numpy.__version__)
+print("Pandas:", pandas.__version__)
+print("Matplotlib:", matplotlib.__version__)
+print("SciPy:", scipy.__version__)
+print("Scikit-learn:", sklearn.__version__)
+
+print("すべて正常に読み込めました！")
